c0757646a19e464dfa91fd2c175ce07d8a54d31e
Laiten <laiten808@gmail.com>
2017-07-20 23:32:00 +0400

    -

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp	= require('gulp'),
+	sass	= require('gulp-sass'),
+	del		= require('del'),
+	rigger	= require('gulp-rigger');
+
+//Дефолтный таск ---------------------------------------------------+
+// gulp.task('default', ['clean', 'sass']);
+
+//Работа со стилями ------------------------------------------------+
+// gulp.task('sass', ()=> {
+// 	gulp.src(['', 'css/*.scss']) //выбираем все исходники
+// 		.pipe(sass({outputStyle:'expanded'}).on('error',sass.logError)) //компилируем и выводим ошибки
+// 		.pipe(gulp.dest('build/css'));
+// });
+
+//Чистка -----------------------------------------------------------+
+gulp.task('clean', ()=> {
+	del.sync('build/*');
+});
+
+gulp.task('test', ()=> {
+	console.log("test!!!");
+});
+
+var path = {
+    build: { //Тут мы укажем куда складывать готовые после сборки файлы
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/',
+        fonts: 'build/fonts/'
+    },
+    src: { //Пути откуда брать исходники
+        html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        js: 'src/main.js',
+        style: 'src/**/.scss',
+        img: 'src/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+        html: 'src/*.html',
+        js: 'src/**/*.js',
+        style: 'src/**/*.scss',
+        img: 'src/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    //clean: './build'
+};
+
+// Сборка html -----------------------------------------------------+
+gulp.task('html:build', function () {
+    gulp.src(path.src.html) //Выберем файлы по нужному пути
+        .pipe(rigger()) //Прогоним через rigger
+        .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
+        //.pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений
+});
+
+// Сборка js
+gulp.task('js:build', function () {
+    gulp.src(path.src.js) //Найдем наш main файл
+        .pipe(rigger()) //Прогоним через rigger
+        .pipe(sourcemaps.init()) //Инициализируем sourcemap
+        .pipe(uglify()) //Сожмем наш js
+        .pipe(sourcemaps.write()) //Пропишем карты
+        .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
+        .pipe(reload({stream: true})); //И перезагрузим сервер
+});
