cd06fd4a7fcb8095f802a1cec4616580347f1b4a
Alexey <laiten808@gmail.com>
2017-08-22 17:43:59 +0400

    -

--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@
 //=include lib/jquery-3.1.1.min.js
 //=include lib/jquery.knob.min.js
 
+//=include page8/__box-item/__item-circle/page8__circle.js
+
 console.log(' main.js');
+
+
+
+
---
+++ src/page8/__box-item/__item-circle/page8__circle.js
@@ -0,0 +1,36 @@
+console.log('page8__circle.js starts!');
+
+/* jq | Настройки круговой диаграммы knob.js */
+$(function() {
+	$(".dial").knob({
+	    'min':0, //минимальное значение
+	    'max':100, //максимальное значение
+	    'angleArc': 360, //размер дуги в градусах
+	    'angleOffset':1, //поворот диаграммы в градусах
+    	'width': 174, //ширина
+    	'height': 174, //высота
+    	'fgColor': '#e93b90', //цвет заполняющий шкалы
+    	'bgColor': '#efefef', //цвет фона пустой части диаграммы
+    	'thickness': 0.07, //толщина
+    	'fontWeight': false, //жирность шрифта
+    	'readOnly': true //отключить онлайн взаимодействие с диаграммой
+	});
+});
+
+/* js | Скрипт добавления и удаления классов */
+(function() {
+	var dial = document.querySelectorAll('.dial'); //получить все элементы классв .dial
+	var dialArr = Array.prototype.slice.call(dial); //переводим их в массив
+	dialArr.forEach(function(index, array, currentValue) { //перебор массива
+		var val = index.getAttribute('value'); //получаем значение атрибута
+		if (val >= 100) { 
+			var parent = index.parentNode; //определяем родителя
+			parent.classList.add('hundred'); //добавляем родителю класс
+			index.classList.add('dial_marging'); //добавляем текщ элементу класс
+		} else if (val < 100) {
+			var parent = index.parentNode;
+			parent.classList.remove('hundred'); //удаляем у родителя класс
+			index.classList.remove('dial_marging'); //удаляем у текщ элемента класс
+		} else { return; }
+	});
+})();
