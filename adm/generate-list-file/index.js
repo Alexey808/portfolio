06b65ec144f9345b2150617cb4a989bf1420c350
Alexey <laiten808@gmail.com>
2017-09-07 14:54:10 +0400

    -

--- adm/generate-list-file/index.js
+++ adm/generate-list-file/index.js
@@ -8,16 +8,32 @@ var fs = require('fs');
 var path = require('path');
 
 var getFiles = function (dir, files_){
-    
   files_ = files_ || [];
     var files = fs.readdirSync(dir);
     for (var i in files){
         var name = dir + '/' + files[i];
         if (fs.statSync(name).isDirectory()){
             getFiles(name, files_);
-        } else {
-            files_.push(name);
+        } 
+
+        /* --- все расширения --- */
+        //files_.push(name);
+        
+        /* --- для .js --- */
+        // else if (name.toString().indexOf('.js')!== -1) {
+        //   files_.push(name);
+        // } 
+
+        /* --- для .scss --- */
+        else if (name.toString().indexOf('.scss')!== -1) {
+          files_.push(name);
         }
+
+        /* --- для .html --- */
+        // else if (name.toString().indexOf('.html')!== -1) {
+        //   files_.push(name);
+        // }
+
     }
     return files_;
 };
@@ -34,8 +50,10 @@ if (fs.existsSync(process.cwd() + myCatalog)) {
 
 fs.writeFileSync("get_files.txt", 
     mf.toString()
-      .split(process.cwd())
-      .join('\n')
+      .split(process.cwd() + '/')
+      .join('\n' + "'")
       .split(',')
+      .join("';")
+      .split('.scss')
       .join('')
 );
